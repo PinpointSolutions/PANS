5a1dee344bd00c419192ca0e292827a266bcaf0c
Xavier Ho <contact@xavierho.com>
2011-05-18 22:57:08 +1000

    Fixed the path in Project Configuration.class.php

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__FILE__).'\..\..\symfony\lib\autoload\sfCoreAutoload.class.php';
+require_once dirname(__FILE__).'/../../symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
