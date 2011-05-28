1c82f22835e5c1f4612aecf92fd14544862d02c1
Xavier Ho <contact@xavierho.com>
2011-05-28 22:25:50 +1000

    Added CSS Reset and a favicon.

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,22 +1,28 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html> <!-- Leave it as this so it is HTML5 compatible. -->
+<html lang="en">
   <head>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
-    
-	<title><?php include_slot('title') ?></title>
+    <title><?php include_slot('title') ?></title>
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
   </head>
-  <body>
-  <div id="header">
-     <h1>3001ICT - Project Nomination Form</h1>
-	 <div id="logo"></div>
-     <!--<img id="logo" src="images/griffith_logo.png">-->
-	 <span><?php ?></span>
-  </div>
-
-    <?php echo $sf_content ?>
+  
+  <body> <!-- Three row style, with fixed-width content body -->
+    
+    <div id="header">
+      <h1>3001ICT - Project Nomination Form</h1>
+      <div id="logo"></div>
+    </div>
+    
+    <div id="content">
+      <?php echo $sf_content ?>
+    </div>
+    
+    <div id="footer">
+      Developed by Pinpoint Solutions
+    </div>
+      
   </body>
 </html>
