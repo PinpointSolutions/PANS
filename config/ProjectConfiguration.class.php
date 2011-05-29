d7f54f87bf4374a54276ecf5024a17ab24fbdb1c
Xavier Ho <contact@xavierho.com>
2011-05-29 17:12:32 +1000

    Added a log-in page and now the website is secure.

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -7,6 +7,8 @@ class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
-    $this->enablePlugins('sfDoctrinePlugin');
+    $this->enablePlugins(array(
+      'sfDoctrinePlugin',
+      'sfDoctrineGuardPlugin'));
   }
 }
