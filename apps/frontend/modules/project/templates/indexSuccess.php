581bd3d7696fb9b670153059a04e149f0323a6c0
Xavier Ho <contact@xavierho.com>
2011-05-31 10:29:43 +1000

    Added project additional info note in project list

--- apps/frontend/modules/project/templates/indexSuccess.php
+++ apps/frontend/modules/project/templates/indexSuccess.php
@@ -5,6 +5,7 @@
     <div class="projectTitle"><a href="#"><?php echo $project->getProjNum() ?>. <?php echo $project->getTitle() ?></a></div>
     <div class="projectOrganisation"><?php echo $project->getOrganisation() ?></div>
     <div class="projectMajors">(Majors)</div>
+    <div class="projectNotes"><?php if ($project->getHasAdditionalInfo() == 1) { echo "Additional information available."; } ?></div>
     <div class="projectDescription"><?php echo $project->getDescription() ?></div>
   </div>
 <?php endforeach; ?>
