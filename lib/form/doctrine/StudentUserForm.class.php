cbdee9b54f0a3944679e7b22760419bc8b4236a1
Scott Mabey <s.l.mabey@gmail.com>
2011-08-01 19:09:07 -0700

    Began editing the form using the configure method

--- lib/form/doctrine/StudentUserForm.class.php
+++ lib/form/doctrine/StudentUserForm.class.php
@@ -17,5 +17,28 @@ class StudentUserForm extends BaseStudentUserForm
       $this['updated_at'],
       $this['form_completed']
     );
-  }
+	
+	$snum_widget = new sfWidgetFormInputText();
+	$snum_widget->setLabel('Student Number');
+	
+	$first_name_widget = new sfWidgetFormInputText();
+	$first_name_widget->setLabel('First Name');
+	
+	$last_name_widget = new sfWidgetFormInputText();
+	$last_name_widget->setLabel('Last Name');
+	
+	$pass_fail_widget = new sfWidgetFormInputCheckbox();
+	$pass_fail_widget->setLabel('Please check this box if you passed Project Management: ');
+	
+	$majors_widget = new sfWidgetFormInputText();
+	$majors_widget->setLabel('Indicate your Major/s');	
+	
+	$this->setWidgets(array(
+		'snum' => $snum_widget,
+		'first_name' => $first_name_widget,
+		'last_name' => $last_name_widget,
+		'pass_fail_pm' => $pass_fail_widget,
+		'major_ids' => $majors_widget
+		));
+  }  
 }
