a568ad67617b08b5240ad8f8893afbbf6af6e7c1
Scott Mabey <s.l.mabey@gmail.com>
2011-08-29 17:08:44 -0700

    Attempted to add a backend barely works

---
+++ apps/backend/modules/student/lib/studentGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * student module helper.
+ *
+ * @package    PANS
+ * @subpackage student
+ * @author     Pinpoint Solutions
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class studentGeneratorHelper extends BaseStudentGeneratorHelper
+{
+}
