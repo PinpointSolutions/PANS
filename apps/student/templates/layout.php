2ad811a3c99f20802c15fb6b28817e4e08f0e5f8
Xavier Ho <contact@xavierho.com>
2011-09-01 12:51:59 +1000

    Deleted student.txt and fixed the page title.'

--- apps/student/templates/layout.php
+++ apps/student/templates/layout.php
@@ -3,7 +3,7 @@
   <head>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
-    <title><?php include_slot('title') ?></title>
+    <title>Project Allocation and Nomination System</title>
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
