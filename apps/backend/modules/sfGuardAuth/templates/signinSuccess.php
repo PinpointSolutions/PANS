caf4c331e5ebfde7d716685065febb54a8b7770b
unknown <Justin@.(none)>
2011-09-28 19:53:31 +1000

    Changed admin bg and success_signout_url in app.yml

--- apps/backend/modules/sfGuardAuth/templates/signinSuccess.php
+++ apps/backend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -1,5 +1,5 @@
 <?php use_helper('I18N') ?>
 
-<h1><?php echo __('Please sign in.', null, 'sf_guard') ?></h1>
+<h1><?php echo __('Administrator Login', null, 'sf_guard') ?></h1>
 
 <?php echo get_partial('sfGuardAuth/signin_form', array('form' => $form)) ?>
