a1db70d2ab7d20d4b2c8e90015897bcc21d9e870
Xavier Ho <contact@xavierho.com>
2011-10-02 17:20:36 +1000

    Moving Import Students From File link into Admin View / Tools

--- apps/backend/modules/project/templates/toolSuccess.php
+++ apps/backend/modules/project/templates/toolSuccess.php
@@ -4,6 +4,13 @@
 <?php endif; ?>
 <div class="flash_notice">Actions here cannot be undone. Proceed with caution.</div>
 
+<h2>Import Students From File</h2>
+<form enctype="multipart/form-data" action="<?php echo url_for('@importStudents') ?>" method="POST">
+<input type="hidden" name="MAX_FILE_SIZE" value="1000000" />
+Select a CSV File.  (max. file size: 1MB) <br><br><input name="studentFile" type="file" /><br><br>
+<input type="submit" value="Import" />
+</form>
+
 <ul>
 <li class="tool"><?php echo link_to('Delete All Students', 'project/clearAllStudents') ?></li>
 <li class="tool"><?php echo link_to('Delete All Projects', 'project/clearAllProjects') ?></li>
