e82d760c6b886fbf224061d110321e1e41d59f8d
Xavier Ho <contact@xavierho.com>
2011-10-09 04:28:19 +1000

    Removed add student script -- we can use the site instead.

--- add_students.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-while read -r -a file; do
-# file[*] arrays are for email adress, username, password, first name, last name.
-	php symfony guard:create-user ${file[0]} ${file[1]} ${file[2]} ${file[3]} ${file[4]}
-done
