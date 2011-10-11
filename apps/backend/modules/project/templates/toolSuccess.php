64b235a9736321dfc1fd46609d13f0537ccf0d3e
Xavier Ho <contact@xavierho.com>
2011-10-11 13:43:55 +1000

    Added a link to edit student users if the admin wishes just to change one password

--- apps/backend/modules/project/templates/toolSuccess.php
+++ apps/backend/modules/project/templates/toolSuccess.php
@@ -47,6 +47,7 @@ Deadline:
 
 <h2>Reset and Email All Passwords</h2>
 <form enctype="multipart/form-data" onSubmit="return confirm('This will erase all existing student passwords and generate whole new ones. Are You Sure?')" action="<?php echo url_for('project/emailAllPasswords') ?>" method="POST" class="tool-form">
+  Note: To reset one user's password, edit them  <?php echo link_to("here", 'guard/users'); ?>  instead.<br><br>
   This will reset every students' passwords.  Emails will be sent out with their new ones.<br><br>
   <table>
     <tr>
