7f1b4a311a225bdbed894ae9349324e23d49cbb8
Xavier Ho <contact@xavierho.com>
2011-10-18 10:11:45 +1000

    Added new Email generated files

---
+++ lib/form/doctrine/EmailForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Email form.
+ *
+ * @package    PANS
+ * @subpackage form
+ * @author     Pinpoint Solutions
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class EmailForm extends BaseEmailForm
+{
+  public function configure()
+  {
+  }
+}
